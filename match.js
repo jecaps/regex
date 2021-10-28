0f17a423a73735d4e99eaba3f309f278332255c5
Jerika Kanz <jerikacapila@gmail.com>
2021-10-28 10:49:05 +0200

    Find More Than the First Match

--- match.js
+++ match.js
@@ -17,3 +17,18 @@ console.log(ourStr.match(ourRegex));
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
 console.log(extractStr.match(codingRegex));
+
+/* To search or extract a pattern more than once, you can use the g flag.
+  Note that you can have multiple flags on your regex like /code/gi*/
+
+let testStr = "Repeat, Repeat, Repeat";
+let ourRegexp = /Repeat/;
+console.log(testStr.match(ourRegexp));
+let repeatRegex = /Repeat/g;
+console.log(testStr.match(repeatRegex));
+
+// Challenge
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; 
+console.log(twinkleStar.match(starRegex);
